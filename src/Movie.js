ccb247025f3d432f17791df029e503bc3d5a0677
ARONGLEE <arong16@naver.com>
2021-08-29 03:08:30 +0900

    movie app
    genre add, css add

--- src/Movie.js
+++ src/Movie.js
@@ -8,9 +8,9 @@ function Movie({ year, title, summary, poster, genres}){
         <div className="movie_data">
         <h3 className="movie_title">{title}</h3>
         <h5 className="movie_year">{year}</h5>
-        <ul className="genres">{genres.map((genre, index) => 
-            (<li className="genres_genre">{index}{genre}</li>))}</ul>
-        <p className="movie_summary">{summary}</p>
+        <ul className="movie_genres">{genres.map((genre, index) => 
+            (<li key={index} className="genres_genre">{genre}</li>))}</ul>
+        <p className="movie_summary">{summary.slice(0, 140)}...</p>
         </div>
     </div>
 }
